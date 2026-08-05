821624e08815139f0874e4439e62016a72d53b6e
Saber <16295052+saberfl123@user.noreply.gitee.com>
2026-08-05 11:55:10 +0800

    完成

--- layerd/src/com/saber/layerd/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.saber.layerd.service;
-
-public interface UserService
-{
-    public String register(String username, String password);
-
-    public String login(String username, String password);
-
-}
